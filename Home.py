ad970e100b3087f61ee6e54e24a568f6c871b8bb
Charanvir <charanvir123@gmail.com>
2023-03-31 15:28:14 -0400

    created home page, with status on progress for a records graph to track units won/loss throughout the year, and potentially throughout the history of the company

--- Home.py
+++ Home.py
@@ -1,3 +1,16 @@
 import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
+from PIL import Image
+
+st.set_page_config(layout="wide")
+
+st.header("Farlo MLB Model")
+
+st.subheader("Record this season:")
+
+st.write("This is where we will update our record and a graph showing the units won/loss")
+
+image = Image.open("assets/workinprogress.jpeg")
+
+st.image(image)
